525a0e54e1ab1de89f9162031bcce0c44b6a72b5
JB Tellez <jb.tellez@gmail.com>
2018-07-23 12:47:35 -0700

    solves kata

---
+++ replace-all-dots/solution.js
@@ -0,0 +1,14 @@
+var replaceDots = function(str) {
+
+  let newStr = '';
+  
+  for (let i = 0; i < str.length; i++) {
+    if(str[i] === '.') {
+      newStr += '-';
+    } else {
+      newStr += str[i];
+    }
+  }
+  
+  return newStr;
+ }
